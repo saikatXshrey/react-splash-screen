2858c224069f12a46f5c1cba0ea6beee2e8dcadd
saikatXshrey <saikatxshrey@gmail.com>
2021-06-04 04:13:27 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import PacmanLoader from "react-spinners/PacmanLoader";
+import ClockLoader from "react-spinners/ClockLoader";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
+  const override = `
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+  return isLoading ?
+    <PacmanLoader color={'#36D7B7'} isLoading={isLoading} css={override} size={150} /> :
+    <h1 className="App">
+      This is Main Page
+      {<ClockLoader color={'#36D7B7'} isLoading={isLoading} css={override} size={150} />}
+    </h1>
 }
 
 export default App;
